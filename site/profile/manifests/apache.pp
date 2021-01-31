47d395ed2dd1789cc46f47aa6ce82b47307e65ab
neha12191 <60854924+neha12191@users.noreply.github.com>
2021-01-31 17:37:08 +0000

    added modules

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -2,4 +2,8 @@ class profile::apache {
    class {'::apache':
 	serveradmin => 'u2004643@uel.ac.uk',
     }
+	apache::vhost { 'vhost.example.com':
+	   port => '80',
+	   docroot => '/var/www/vhost',
+	}
 }
