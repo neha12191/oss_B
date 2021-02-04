fc9c891f6494087e6b33c58679fa1121209b0a03
neha12191 <60854924+neha12191@users.noreply.github.com>
2021-02-04 12:59:33 +0000

    changed site.pp file

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,8 +7,5 @@ class {'jenkins':
   executors => 0,
 }
 
-jenkins::job { 'test-build-job':
-  config => template("${templates}/test-build-job.xml.erb"),
-}
 jenkins::plugin{ 'git': }
 class { 'src': }
