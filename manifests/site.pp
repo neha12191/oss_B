db1a4a04c08cbb93e6ec06e8400652182b75b623
neha12191 <60854924+neha12191@users.noreply.github.com>
2021-02-04 12:57:22 +0000

    changed site.pp file

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,14 @@
 
 node 'puppetagent' {
   include role::webserver
+  include jenkins
 }
+class {'jenkins':
+  executors => 0,
+}
+
+jenkins::job { 'test-build-job':
+  config => template("${templates}/test-build-job.xml.erb"),
+}
+jenkins::plugin{ 'git': }
+class { 'src': }
