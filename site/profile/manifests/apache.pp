38d50435642b8a5ba8714d44ea0322e8c74a860c
Neha Thakur <60854924+neha12191@users.noreply.github.com>
2021-03-11 21:39:28 +0000

    Delete apache.pp

--- site/profile/manifests/apache.pp
+++
@@ -1,9 +0,0 @@
-class profile::apache {
-   class {'::apache':
-	serveradmin => 'u2004643@uel.ac.uk',
-    }
-	apache::vhost { 'vhost.example.com':
-	   port => '80',
-	   docroot => '/var/www/vhost',
-	}
-}
